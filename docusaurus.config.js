5e852b560a64af9db97b0a0481648e6df6e27268
Grange <2634070476@qq.com>
2024-03-11 18:08:19 +0800

    🎉 First commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tinkerfuroc.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tinkerdocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Tinker@FuRoC', // Usually your GitHub org/user name.
-  projectName: 'Tinker_Docs', // Usually your repo name.
-
+  organizationName: 'tinkerfuroc', // Usually your GitHub org/user name.
+  projectName: 'Tinker_docs', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
