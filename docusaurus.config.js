841490a551e4c1e607232841c0726be4f272bb0f
Grange <2634070476@qq.com>
2024-03-12 20:33:50 +0800

    🐛 Fix Github Link Bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/tinkerfuroc/tinkerdocs',
             label: 'GitHub',
             position: 'right',
           },
